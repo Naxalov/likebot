db3f7b2d2c9fa1061ddb40098032cb11ff945a7d
Otabek <ojuraev@gmail.com>
2021-08-15 12:07:28 +0900

    some changes

--- test.py
+++ test.py
@@ -1,37 +1,28 @@
 import json
 from json.decoder import JSONDecodeError
 
-def add_likes(chat_id, like, dislike):
-    pass
-def add_newuser_likes(chat_id, like, dislike):
-    pass
 
-
-dict1 = {}
-try:
-    dict1 = json.load(open("data_file.json"))
-    # dict1 = {}
-    print(dict1)
-except JSONDecodeError:
+def db_update (chat_id, likes, dislikes):
     dict1 = {}
-    pass
+    try:
+        dict1 = json.load(open("data_file.json"))
 
-likes = 1
-dislikes =1
-chat_id =1
-for i in range(11,15):
-    if dict1.get(str(chat_id+i)) is not None:
-        dict1[str(chat_id+i)] = {"likes": likes, "dislikes": dislikes}
+        print(dict1)
+    except JSONDecodeError:
+        dict1 = {}
+        pass
+
+ 
+    if dict1.get(str(chat_id)) is not None:
+        dict1[str(chat_id)] = {"likes": likes, "dislikes": dislikes}
+        print("Yangi ID")
     else:
-        dict1[str(chat_id+i)] = {"likes": 0, "dislikes": 0}
+        dict1[str(chat_id)] = {"likes": dict1[chat_id][likes] + 1, "dislikes": dict1[chat_id][likes] + 1}
+        print("Mavjud Id update")
+    y =json.dumps(dict1)
+    f =open("data_file.json", "w")
+    
+    f.write(y)
+    f.close()
 
-y =json.dumps(dict1)
-f =open("data_file.json", "w")
-print("Done writing")
-f.write(y)
-f.close()
-
-a = {11111111:{"likes": likes+i, "dislikes": dislikes+i}}
-
-
-
+db_update(10, 5,7)
